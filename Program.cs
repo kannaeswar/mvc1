0d3466dad9ff7db2d9d6c450472d6ca107ea440e
kannaeswar <kannaeswar@gmail.com>
2014-01-08 00:50:32 -0500

    Initial files added
    Initial files added

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailSenderMercury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SmtpClient s;
+                MailMessage msg;
+
+                s = new SmtpClient();
+                msg = new MailMessage();
+                //s.Host = "smtp.ihostexchange.net";
+                //s.Port = 54566; 
+                s.UseDefaultCredentials = true;
+                s.Credentials = new NetworkCredential("support", "");
+                s.EnableSsl = false;           
+                msg.IsBodyHtml = true;
+                msg.Body = "hello ";
+                msg.Subject = "website";
+                msg.To.Add("");
+                msg.From = new MailAddress("");
+                s.Send(msg);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
